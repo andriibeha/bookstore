bca85db6a4c1e659f0c38c31b63d9f3fa6238cfb
Andrii Beha <95135544+andriibeha@users.noreply.github.com>
2023-12-13 16:13:00 -0700

    add method for updating a book

--- backend/index.js
+++ backend/index.js
@@ -39,12 +39,62 @@ app.post('/books', async (req, res) => {
 app.get('/books', async (req, res) => {
 	try {
 		const books = await Book.find({})
-		console.log(typeof books)
 
 		return res.status(200).json({
 			count: books.length,
 			data: books,
 		})
+	} catch (error) {
+		console.log(error.message)
+		res.status(500).send({ message: error.message })
+	}
+})
+
+//Routes GET one Book
+app.get('/books/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+
+		const book = await Book.findById(id)
+
+		if (!book) {
+			return res.status(404).json({ message: 'Book not found' })
+		}
+
+		return res.status(200).json(book)
+	} catch (error) {
+		console.log(error.message)
+		res.status(500).send({ message: error.message })
+	}
+})
+
+//Routes PUT one Book
+//Routes PUT one Book
+app.put('/books/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+
+		if (!req.body.title || !req.body.author || !req.body.publishYear) {
+			return res.status(400).send({
+				message: 'Send all required fields: title, author, publishYear',
+			})
+		}
+
+		const updateData = {
+			title: req.body.title,
+			author: req.body.author,
+			publishYear: req.body.publishYear,
+		}
+
+		const result = await Book.findByIdAndUpdate(id, updateData, { new: true })
+
+		if (!result) {
+			return res.status(404).json({ message: 'Book not found' })
+		}
+
+		return res
+			.status(200)
+			.send({ message: 'Book updated', updatedBook: result })
 	} catch (error) {
 		console.log(error.message)
 		res.status(500).send({ message: error.message })
