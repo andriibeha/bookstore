61f0e2af85768d81ec1bd0911f6c96bed926e392
Andrii Beha <95135544+andriibeha@users.noreply.github.com>
2023-12-15 14:40:04 -0700

    add delete book

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,5 +1,50 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import BackButton from '../components/BackButton'
+import Spiner from '../components/Spiner'
+
 const DeleteBook = () => {
-	return <div>DeleteBook</div>
+	const [loading, setLoading] = useState(false)
+
+	const navigate = useNavigate()
+
+	const { id } = useParams()
+
+	const handleDeleteBook = () => {
+		setLoading(true)
+
+		axios
+			.delete(`http://localhost:5555/books/${id}`)
+			.then(() => {
+				setLoading(false)
+				navigate('/')
+			})
+			.catch(error => {
+				console.log(error)
+				setLoading(false)
+			})
+	}
+
+	return (
+		<div className='py-4'>
+			<BackButton />
+			<h1 className='text-3xl my-4'>Delete Book</h1>
+
+			{loading ? <Spiner /> : ''}
+
+			<div className='flex flex-col border-2 vorder-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+				<h3 className='text-2xl'>Delete the book?</h3>
+
+				<button
+					className='p-4 bg-red-600 text-white m-8 '
+					onClick={handleDeleteBook}
+				>
+					Delete
+				</button>
+			</div>
+		</div>
+	)
 }
 
 export default DeleteBook
